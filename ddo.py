10df36aa309c03ad8df511a379a619830d0963a6
junseoh729 <junseoh729@gmail.com>
2025-05-07 20:14:22 +0900

    Update ddo.py

--- ddo.py
+++ ddo.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
+col1, col2 = st.columns([2,3])
+with coll :
+    st.title("here is column1 title")
 
-st.title('this is title')
-st.header('this is header')
-st.subheader('this is subheader')
+with col2 :
+    st.title("here is column2 tltle")
+    st.checkbox("this is checkbox1 in col2")
+col1.subheader("i am column1 subheader")
+col2.checkbox("this is checkbox2 in col2")
